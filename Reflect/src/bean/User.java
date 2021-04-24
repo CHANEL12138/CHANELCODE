3d39abaca2ba7285ceb9ad5d2aa2096d0525dbae
CHANEL12138 <40346942+CHANEL12138@users.noreply.github.com>
2021-04-24 17:28:46 +0800

    1 1 1

--- Reflect/src/bean/User.java
+++ Reflect/src/bean/User.java
@@ -1,8 +1,19 @@
 package bean;
 
 public class User {
+
+    public String Name;
+    public int Number;
+    public String sex;
+    private boolean isLive;
+
+
     public User()
     {
         System.out.println("调无参构造方法");
     }
+
+
+
+
 }
